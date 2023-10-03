7456715b0bbadc1efe18ad3450ae89b84171c295
avizarraga <avizarraga@106-29.info2>
2023-10-03 13:40:10 +0200

    Ejercicio 1 terminado

--- src/ejerciciost2/Ejercicio1.java
+++ src/ejerciciost2/Ejercicio1.java
@@ -1,10 +1,24 @@
 package ejerciciost2;
+import java.util.Scanner;
 
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		int firstValue, result = 0;
+		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.print("Introduzca un valor entero: ");
+		firstValue = sc.nextInt();
+		
+		result = firstValue % 2;
+		
+		if (result == 0) {
+			System.out.println("Tu valor es par.");
+		} else {
+			System.out.println("Tu valor es impar.");
+		}
+		
+		sc.close();
 	}
 
 }
