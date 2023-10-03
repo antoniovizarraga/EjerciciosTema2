bbcad1e2662b040494afd00736f30001747a3ef6
avizarraga <avizarraga@106-29.info2>
2023-10-03 13:51:28 +0200

    Ejercicio 2 terminado

--- src/ejerciciost2/Ejercicio2.java
+++ src/ejerciciost2/Ejercicio2.java
@@ -2,10 +2,34 @@ package ejerciciost2;
 import java.util.Scanner;
 
 public class Ejercicio2 {
-
+	
+	/* Crearemos un código que compruebe si dos valores introducidos
+	 * son iguales o no. Para luego decirle al usuario el resultado. */
 	public static void main(String[] args) {
+		// Creamos los dos valores que le pediremos al usuario.
+		int firstValue, secondValue = 0;
 		
-
+		// Creamos el Scanner.
+		Scanner sc = new Scanner(System.in);
+		
+		// Le pedimos los valores al usuario.
+		System.out.print("Introduce un valor entero: ");
+		firstValue = sc.nextInt();
+		
+		System.out.print("Introduce el segundo valor entero: ");
+		secondValue = sc.nextInt();
+		
+		
+		/* Comprobamos si los dos valores son iguales. En caso de serlo,
+		 * hazle saber al usuario que son iguales, y viceversa. */
+		if(firstValue == secondValue) {
+			System.out.println("Los dos valores introducidos son iguales.");
+		} else {
+			System.out.println("Los dos valores introducidos no son iguales.");
+		}
+		
+		// Cerramos el Scanner.
+		sc.close();
 	}
 
 }
